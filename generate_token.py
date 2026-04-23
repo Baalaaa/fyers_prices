b89c686154ac9ab75c6b5d9f8359d954ccb96f11
Balakrishna <balakrishna.bobala.dev@gmail.com>
2026-04-23 16:33:55 +0530

    Third code commit

--- generate_token.py
+++ generate_token.py
@@ -1,9 +1,12 @@
 import os
 
-from fyers_apiv3 import fyersModel
+from loggers import logger
 from dotenv import load_dotenv
+from fyers_apiv3 import fyersModel
 
-# --- Load .env Files ---
+
+
+# --- Loading .env Files ---
 load_dotenv()
 
 
@@ -24,7 +27,7 @@ class FyersToken:
         )
 
 
-    # --- Generate Url Func ---
+    # ---- Generate URL Func ----
     def generate_url(self):
         """Generate Login URL for Fyers API"""
 
@@ -32,8 +35,7 @@ class FyersToken:
         print(login_url)
 
 
-
-    # ---- Access Token Func ----
+    # ---- Access Token Func -----
     def generate_access_token(self, auth_code: str):
         """Generate access token for Fyers API"""
 
@@ -42,29 +44,31 @@ class FyersToken:
 
             # Generate Access Token
             response = self.session.generate_token()
-            print(response)
+            logger.info(response.get('s'))
+
             if response.get("s") != "ok":
-                print("Error:", response)
+                logger.error(f"Error: {response}")
                 return
 
             access_token = response['access_token']
-            print(f"Access Token Generated: {access_token} !")
+            logger.info(f"Access Token Generated: {access_token} !")
 
             # Save it
             with open("access_token.txt", "w") as file:
                 file.write(access_token)
 
         except Exception as e:
-            print(e)
+            logger.error("Exception occurred while generating access_token:", e)
+
+
+
+
 
 if __name__ == '__main__':
 
-    # 1
-    fy_obj = FyersToken()
 
-    # 2
+    fy_obj = FyersToken()
     fy_obj.generate_url()
 
     auth_code = input("Enter Authorization Code: ")
-    #3
     fy_obj.generate_access_token(auth_code)
